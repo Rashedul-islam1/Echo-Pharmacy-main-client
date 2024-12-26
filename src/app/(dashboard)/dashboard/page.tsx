d85bafc1e0ab729a4f5125d7d38ee2400f548a68
rashednsu172@gmail.com <Rashedul islam>
2024-12-26 18:24:29 +0600

    update dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -93,29 +93,12 @@ const Dashboard = () => {
           </div>
           <div className="text-right">
             <p className="text-2xl">{products?.data?.length}</p>
-            <p>Total Product</p>
+            <p>Total Products</p>
           </div>
         </div>
 
         {/* Premium Subscriptions */}
         <div className="bg-primary shadow-lg rounded-md flex  justify-between p-3 text-white font-medium group">
-          <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-            <svg
-              width="30"
-              height="30"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="stroke-current text-primary transform transition-transform duration-500 ease-in-out"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
-          </div>
           <div className="text-right">
             <p className="text-2xl">{premiumUser}</p>
             <p>Premium Sub</p>
