939a8862e77a8d0fc5d575a1ce37096e00b2001f
rashednsu172@gmail.com <Rashedul islam>
2024-12-26 18:26:27 +0600

    update dashboard

--- src/app/(dashboard)/manage-orders/page.tsx
+++ src/app/(dashboard)/manage-orders/page.tsx
@@ -89,10 +89,6 @@ const ManageProducts = () => {
                     {order.status === "completed" ? order.status : "complete"}
                   </button>
                   <button
-                    disabled={
-                      order.status === "cancelled" ||
-                      order.status === "completed"
-                    }
                     onClick={() => handleChangeStatus(order._id, "cancelled")}
                     className={`inline-block px-3 py-1 text-center capitalize ${
                       order.status === "shipping"
